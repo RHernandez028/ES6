daa7588821c809f24f24b09466af865ff452d05e
Gabriel <rhernadnez028@west-mec.org>
2019-08-26 18:22:49 -0700

    Added es6 js

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,24 +1,49 @@
 "use strict";
 
-var a = 'Test1';
-var b = 'Test2';
+// var a = 'Test1';
+// var b = 'Test2';
 
-console.log(`Logged: ${a}`);
+// console.log(`Logged: ${a}`);
 
-const testVar = (params) => {
-    let a = 30;
-    console.log(`Function Logged: ${a}`);
+// const testVar = (params) => {
+//     let a = 30;
+//     console.log(`Function Logged: ${a}`);
 
-    if (true) {
-         var a = 50; 
-         console.log(`Block Logged:  ${a}`);  
+//     if (true) {
+//          var a = 50; 
+//          console.log(`Block Logged:  ${a}`);  
+//     }
+//     console.log("here: " + a);
+// }
+// testVar();
+
+// console.log(`END Logged: ${a}`);
+
+let LOG = console.log();
+
+const pizza = [
+    {
+        name: "pep",
+        number: 3,
+        price: 5
+
+    },
+    {
+        name: "mushroom",
+        number: 3,
+        price: 5
+
+    },
+    {
+        name: "mango",
+        number: 3,
+        price: 5
+
     }
-    console.log("here: " + a);
-}
-testVar();
+];
 
-console.log(`END Logged: ${a}`);
+const [name] = pizza;
 
-
+LOG(pizza);
 
 
